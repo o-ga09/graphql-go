45c16c42f8ed73d914897d8becd434999dccadb9
taiti09 <taitiabe1997@gmail.com>
2024-08-15 15:29:49 +0900

    依存性注入

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"log/slog"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/o-ga09/graphql-go/db"
+	"github.com/o-ga09/graphql-go/db/dao"
 	"github.com/o-ga09/graphql-go/graph"
+	"github.com/o-ga09/graphql-go/pkg/logger"
+	"github.com/o-ga09/graphql-go/service"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	ctx := context.Background()
+	logger.New()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	conn := db.Connect(ctx)
+	if conn == nil {
+		slog.Log(ctx, logger.SeverityError, "failed to connect db")
+		return
+	}
+	noteRepo := dao.NewNoteDao(conn)
+	userRepo := dao.NewUserDao(conn)
+	noteService := service.NewNoteService(noteRepo)
+	userService := service.NewUserService(userRepo)
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		NoteService: *noteService,
+		UserService: *userService,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	slog.Log(ctx, logger.SeverityInfo, fmt.Sprintf("connect to http://localhost:%s/ for GraphQL playground", port))
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
